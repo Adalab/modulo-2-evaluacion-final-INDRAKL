7a9568330caf7fc24e6bcbedd3af0e5a891ad47f
Indra Kishinchand López <indrakislo@gmail.com>
2024-01-03 09:46:51 +0000

    placeholder photo

--- src/js/main.js
+++ src/js/main.js
@@ -5,9 +5,13 @@ const charactersList = document.querySelector(".js_charactersList");
 let characters = [];
 
 function renderOne(charactersData) {
+  let imageUrl =
+    charactersData.imageUrl ||
+    "https://via.placeholder.com/210x295/ffffff/555555/?text=Disney";
+
   charactersList.innerHTML += `
   <li class="characters__li">
-    <img src="${charactersData.imageUrl}" class="characters__img" alt="${charactersData.name}">
+    <img src="${imageUrl}" class="characters__img" alt="${charactersData.name}">
     <h4 class="characters__name">${charactersData.name}</h4>
     </li>`;
 }
