9e16f2721c3c7cdada223c8f4cabb9a03686b218
Indra Kishinchand López <indrakislo@gmail.com>
2024-01-03 00:16:11 +0000

    add main js

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,29 @@
 "use strict";
+
+const charactersList = document.querySelector(".js_charactersList");
+
+let characters = [];
+
+function renderOne(charactersData) {
+  charactersList.innerHTML += `
+      <li class="">
+        <h3>${charactersData.name}</h3>
+        <ul class="colors">
+        </ul>
+      </li>
+    `;
+}
+
+function renderAll(data) {
+  charactersList.innerHTML = "";
+  for (const eachCharacter of characters) {
+    renderOne(eachCharacter);
+  }
+}
+
+fetch("api.disneyapi.dev/character?pageSize=50")
+  .then((response) => response.json())
+  .then((data) => {
+    characters = data;
+    renderAll();
+  });
