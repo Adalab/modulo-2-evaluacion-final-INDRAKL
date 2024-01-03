fcb0a053bef411340e4e631b0ad2612948c6c620
Indra Kishinchand López <indrakislo@gmail.com>
2024-01-03 23:37:41 +0000

    button remove favorites

--- src/js/main.js
+++ src/js/main.js
@@ -6,76 +6,86 @@ const charactersList = document.querySelector(".js_charactersList");
 const charactersListFavorites = document.querySelector(
   ".js_charactersListFavorites"
 );
-const btnSearch = document.querySelector(".js_btnSearch");
 const inputSearch = document.querySelector(".js_inputSearch");
 const form = document.querySelector(".js_form");
+const btnRemove = document.querySelector(".js_btnRemove");
 
 //DATOS
 
 let characters = [];
-const charactersFavorites = [];
+let charactersFavorites = [];
 
 //FUNCIONES
 
-function getImageUrl(characterData) {
-  if (characterData.imageUrl !== undefined && characterData.imageUrl !== null) {
-    return characterData.imageUrl;
-  }
-  return "https://via.placeholder.com/210x295/flo/905/?text=Disney";
-}
+//pinta uno
 
 function renderOne(characterData) {
-  const imageUrl = getImageUrl(characterData);
+  const imageUrl =
+    characterData.imageUrl ||
+    "https://via.placeholder.com/210x295/flo/905/?text=Disney";
   charactersList.innerHTML += `
-    <li class="characters__li js_character data-id="${characterData.id}">
-      <img src="${imageUrl}" class="characters__img" alt="${characterData.name}">
-      <h4 class="characters__name">${characterData.name}</h4>
-    </li>`;
+  <li class="characters__li js_character data-id="${characterData._id}">
+    <img src="${imageUrl}" class="characters__img" alt="${characterData.name}">
+    <h4 class="characters__name">${characterData.name}</h4>
+  </li>`;
 }
 
-function renderOneFavorite(characterData) {
-  const imageUrl = getImageUrl(characterData);
-  charactersListFavorites.innerHTML += `
-  <li class="characters__li js_character data-id="${characterData.id}">
-  <img src="${imageUrl}" class="characters__img" alt="${characterData.name}">
-      <h4 class="characters__name">${characterData.name}</h4>
-    </li>`;
-}
-
-function renderFavorites() {
-  charactersListFavorites.innerHTML = "";
-
-  for (const characterFavorite of charactersFavorites) {
-    renderOneFavorite(characterFavorite);
-  }
-}
+//pintar todos
 
 function renderAll() {
   for (const character of characters) {
     renderOne(character);
   }
   const allCharacters = document.querySelectorAll(".js_character");
-  for (const characterLi of allCharacters)
-    characterLi.addEventListener("click", handleFavorite);
+  for (const oneCharacter of allCharacters) {
+    oneCharacter.addEventListener("click", handlefavorites);
+  }
 }
 
-function handleFavorite(event) {
-  const clickedCharacter = event.currentTarget;
-  const clickedCharacterId = clickedCharacter.dataset.id;
+//pintar un favorito
+
+function renderOneFavorite(characterData) {
+  const imageUrl =
+    characterData.imageUrl ||
+    `https://via.placeholder.com/210x295/flo/905/?text=Disney`;
+  charactersListFavorites.innerHTML += `
+  <li class="characters__li js_character">
+    <img src="${imageUrl}" class="characters__img" alt="${characterData.name}">
+    <h4 class="characters__name">${characterData.name}</h4>
+  </li>`;
+}
+
+//pintar todos los favoritos
+
+function renderFavorites() {
+  charactersListFavorites.innerHTML = "";
+  for (const characterFavorite of charactersFavorites) {
+    renderOneFavorite(characterFavorite);
+  }
+}
+
+// función para agregar favoritos
+
+function handlefavorites(event) {
+  const clickedCharacterLi = event.currentTarget;
+  const clickedCharacterId = clickedCharacterLi.dataset.id;
   const selectedCharacter = characters.find(
-    (oneCharacter) => oneCharacter.id === clickedCharacter.dataset.id
+    (oneCharacter) => oneCharacter._id === parseInt(clickedCharacterId)
   );
-  const favoriteCharacter = charactersFavorites.findIndex(
-    (oneCharacter) => oneCharacter.id === clickedCharacterId
+  const favoritesCharacterIndex = charactersFavorites.findIndex(
+    (onefavorite) => onefavorite._id === parseInt(clickedCharacterId)
   );
-  if (favoriteCharacter === -1) {
+  if (favoritesCharacterIndex === -1) {
+    //No esta en el array de favoritos. Lo pongo!
+
     charactersFavorites.push(selectedCharacter);
   } else {
-    charactersFavorites.splice(favoriteCharacter, 1);
+    // La quito!!
+
+    charactersFavorites.splice(favoritesCharacterIndex, 1);
   }
   renderFavorites();
-
-  clickedCharacter.classList.toggle("favorites");
+  clickedCharacterLi.classList.toggle("favorites");
 }
 
 // EVENTOS
@@ -87,10 +97,16 @@ form.addEventListener("submit", (event) => {
     .then((data) => {
       const searchResults = data.data;
       charactersList.innerHTML = "";
+      charactersListFavorites.innerHTML = "";
       for (const result of searchResults) {
         renderOne(result);
       }
     });
+});
+
+btnRemove.addEventListener("click", (event) => {
+  charactersListFavorites.classList.remove("favorites__li");
+  charactersListFavorites.innerHTML = "";
 });
 
 //CÓDIGO QUE SE EJECUTA AL CARGAR LA PÁGINA
