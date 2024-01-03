d3142758aa52c243b97ee798a5f9139ec1d2f8ac
Indra Kishinchand López <indrakislo@gmail.com>
2024-01-03 13:00:59 +0000

    funtion handle favorites

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,12 @@
 "use strict";
 
 const charactersList = document.querySelector(".js_charactersList");
+const charactersListFavorites = document.querySelector(
+  ".js_charactersListFavorites"
+);
 
 let characters = [];
+let charactersFavorites = [];
 
 function renderOne(charactersData) {
   let imageUrl =
@@ -10,16 +14,41 @@ function renderOne(charactersData) {
     "https://via.placeholder.com/210x295/ffffff/555555/?text=Disney";
 
   charactersList.innerHTML += `
-  <li class="characters__li">
-    <img src="${imageUrl}" class="characters__img" alt="${charactersData.name}">
-    <h4 class="characters__name">${charactersData.name}</h4>
-    </li>`;
+      <li class="characters__li">
+        <img src="${imageUrl}" class="characters__img" alt="${charactersData.name}">
+        <h4 class="characters__name">${charactersData.name}</h4>
+        </li>`;
 }
 function renderAll() {
-  charactersList.innerHTML = "";
   for (const character of characters) {
     renderOne(character);
+    charactersList.addEventListener("click", handleFavorite);
   }
+}
+
+function handleFavorite(event) {
+  const clickedCharacter = event.currentTarget;
+  clickedCharacter.classList.toggle("favorites");
+  const selectedCharacter = characters.find(
+    (oneCharacter) => oneCharacter.id === clickedCharacter.dataset.id
+  );
+  charactersList.innerHTML += `
+        <li class="characters__li">
+          <img src="${selectedCharacter.imageUrl}" class="characters__img" alt="${selectedCharacter.name}">
+          <h4 class="characters__name">${selectedCharacter.name}</h4>
+        </li>`;
+}
+
+function renderFavorite(charactersData) {
+  let imageUrl =
+    charactersData.imageUrl ||
+    "https://via.placeholder.com/210x295/ffffff/555555/?text=Disney";
+
+  charactersListFavorites.innerHTML += `
+    <li class="characters__li" data-id="${charactersData.id}">
+      <img src="${imageUrl}" class="characters__img" alt="${charactersData.name}">
+      <h4 class="characters__name">${charactersData.name}</h4>
+    </li>`;
 }
 
 fetch("//api.disneyapi.dev/character?pageSize=50")
